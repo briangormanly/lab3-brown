5f1619120ec8794f4c2bc8fc5fb685ffb33540e8
brian.gormanly <bgormanly@gmail.com>
2019-05-31 13:38:11 -0400

    starting lab3

---
+++ src/main/java/lab3/constants/Lab3PortletKeys.java
@@ -0,0 +1,10 @@
+package lab3.constants;
+
+/**
+ * @author brian
+ */
+public class Lab3PortletKeys {
+
+	public static final String Lab3 = "lab3";
+
+}
---
+++ src/main/java/lab3/portlet/Lab3Portlet.java
@@ -0,0 +1,28 @@
+package lab3.portlet;
+
+import lab3.constants.Lab3PortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author brian
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + Lab3PortletKeys.Lab3,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class Lab3Portlet extends MVCPortlet {
+}
